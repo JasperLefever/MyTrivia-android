6536560eb344929ab64015f9a56b03ac96eac596
JasperLefever <jasper.lefever@telenet.be>
2023-11-11 14:19:15 +0100

    Category card and screen

--- app/src/main/java/icu/repsaj/android/mytrivia/ui/CategoryOverviewScreen.kt
+++ app/src/main/java/icu/repsaj/android/mytrivia/ui/CategoryOverviewScreen.kt
@@ -1,9 +1,80 @@
 package icu.repsaj.android.mytrivia.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import icu.repsaj.android.mytrivia.ui.theme.spacing
 
 @Composable
 fun CategoryOverviewScreen(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.padding(top = MaterialTheme.spacing.small)) {
+        CategoryCard(name = "Movies")
+        CategoryCard(name = "Movies")
+        CategoryCard(name = "Movies")
+    }
+}
 
+@Composable
+fun CategoryCard(
+    name: String = "",
+    icon: ImageVector = Icons.Filled.Home,
+    modifier: Modifier = Modifier
+) {
+    ElevatedCard(
+        modifier = modifier
+            .padding(
+                horizontal = MaterialTheme.spacing.medium,
+                vertical = MaterialTheme.spacing.small
+            )
+            .fillMaxWidth()
+            .clickable { /*TODO */ },
+
+        ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = name,
+                modifier = Modifier
+                    .size(50.dp)
+                    .padding(end = MaterialTheme.spacing.medium)
+                    .align(Alignment.CenterVertically)
+            )
+
+            Text(
+                text = name,
+                modifier = Modifier
+                    .align(Alignment.CenterVertically),
+                style = MaterialTheme.typography.displayMedium
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Icon(
+                imageVector = Icons.Filled.PlayArrow,
+                contentDescription = name,
+                modifier = Modifier
+                    .size(30.dp)
+                    .align(Alignment.CenterVertically)
+            )
+        }
+    }
 }
