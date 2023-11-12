500b62367f1c2b9fafa2dd9a42b661366282d5cf
JasperLefever <jasper.lefever@telenet.be>
2023-11-12 12:29:02 +0100

    update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -61,12 +61,12 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
     //Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.2")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
     //extra icons
     implementation("androidx.compose.material:material-icons-extended")
 
     //navigation testing
-    androidTestImplementation("androidx.navigation:navigation-testing:2.7.2")
+    androidTestImplementation("androidx.navigation:navigation-testing:2.7.5")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
