16d85e81df500186567867b6dd1ae441bffa8616
JasperLefever <jasper.lefever@telenet.be>
2023-11-12 11:48:00 +0100

    updated category dataclass

--- app/src/main/java/icu/repsaj/android/mytrivia/model/Category.kt
+++ app/src/main/java/icu/repsaj/android/mytrivia/model/Category.kt
@@ -1,7 +1,9 @@
 package icu.repsaj.android.mytrivia.model
 
+import androidx.compose.ui.graphics.vector.ImageVector
+
 data class Category(
     val id: Int,
     val name: String,
-    val imgSrc: String
+    val image: ImageVector
 )
