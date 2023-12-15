cdf625a4a936cd362aa3afd73a801eeb058fd14d
JasperLefever <jasper.lefever@telenet.be>
2023-12-15 16:30:45 +0100

    question repo

--- app/src/main/java/icu/repsaj/android/mytrivia/data/QuestionRepo.kt
+++ app/src/main/java/icu/repsaj/android/mytrivia/data/QuestionRepo.kt
@@ -1,2 +1,26 @@
 package icu.repsaj.android.mytrivia.data
 
+import icu.repsaj.android.mytrivia.model.TriviaQuestion
+import icu.repsaj.android.mytrivia.network.question.IQuestionApiService
+import kotlinx.coroutines.flow.Flow
+
+
+interface QuestionRepo {
+    fun getQuestions(): Flow<List<TriviaQuestion>>
+
+    suspend fun insertQuestion(category: TriviaQuestion)
+
+}
+
+class ApiQuestionRepo(
+    private val questionApiService: IQuestionApiService
+) : QuestionRepo {
+    override fun getQuestions(): Flow<List<TriviaQuestion>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertQuestion(category: TriviaQuestion) {
+        TODO("Not yet implemented")
+    }
+
+}
