96983ae0026885e9b7eda582d4a24bfb1f6bef98
JasperLefever <jasper.lefever@telenet.be>
2023-12-19 14:55:53 +0100

    documentation for entitties

--- app/src/main/java/icu/repsaj/android/mytrivia/data/database/entities/DbCategory.kt
+++ app/src/main/java/icu/repsaj/android/mytrivia/data/database/entities/DbCategory.kt
@@ -5,6 +5,14 @@ import androidx.room.PrimaryKey
 import icu.repsaj.android.mytrivia.model.Category
 import java.util.UUID
 
+/**
+ * Represents a category entity in the database.
+ *
+ * @property id Unique identifier of the category, represented by a UUID.
+ * @property name Name of the category.
+ * @property icon Icon associated with the category. This is the icon string from SF Symbols.
+ * @property questionCount The number of questions available in this category.
+ */
 @Entity(tableName = "categories")
 data class DbCategory(
     @PrimaryKey
@@ -14,6 +22,11 @@ data class DbCategory(
     val questionCount: Int = 0
 )
 
+/**
+ * Converts a database entity [DbCategory] into its domain representation [Category].
+ *
+ * @return A [Category] object with the same properties as the [DbCategory].
+ */
 fun DbCategory.asDomainObject(): Category {
     return Category(
         id = this.id,
@@ -23,6 +36,11 @@ fun DbCategory.asDomainObject(): Category {
     )
 }
 
+/**
+ * Converts a domain object [Category] into a database entity [DbCategory].
+ *
+ * @return A [DbCategory] object with the same properties as the [Category].
+ */
 fun Category.asDbEntity(): DbCategory {
     return DbCategory(
         id = this.id,
@@ -32,6 +50,11 @@ fun Category.asDbEntity(): DbCategory {
     )
 }
 
+/**
+ * Extension function on List<[DbCategory]> to convert a list of database entities into a list of domain objects.
+ *
+ * @return A list of [Category] objects corresponding to the [DbCategory] objects in the list.
+ */
 fun List<DbCategory>.asDomainObjects(): List<Category> {
     return this.map { it.asDomainObject() }
 }
